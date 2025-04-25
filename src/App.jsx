f68a4e30c8b5561c70c94cc504f6c61d38a5612b
mishrajeeeeee <mishrajeeeeee@gmail.com>
2025-04-25 10:55:02 +0530

    ran

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="app">
       <h1>🌿HerbVerse🌿</h1>
+      <h3>The Major update will be rolled out with Home page, Better chat bot,etc. </h3>
       <PlantList /> {/* This will render all plant cards */}
       <FloatingChat />
     </div>
