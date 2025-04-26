e083e531cf2672c3c64bef50826d5cc6c94de465
gaur@v <mishrajeeeeee@gmail.com>
2025-04-26 23:50:59 +0530

    dsolution

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <h3>The Major update will be rolled out with Home page, Better chat bot,
         more plants and main thing the overall UI/UX will be refreshed with indroduction of Credits and Detsils, pls co-operate </h3>
       <PlantList /> {/* This will render all plant cards */}
-      <FloatingChat />
+      {/* <FloatingChat /> */}
     </div>
   );
 }
